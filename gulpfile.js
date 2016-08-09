3352471f7631c6b42e8c7d05c0032b5e252846fe
heathermhargreaves <heathermhargreaves@gmail.com>
2016-08-08 23:30:37 -0600

    added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// gulp.task('hello', function() {
+//   console.log("Hello!");
+// });
+
+
+//example, wait until app is built out to concat all script files
+// gulp.task("concatScripts", function() {
+//   gulp.src([
+//     'JS/main.js',
+//     'JS/jquery.js'
+//   ])
+//   .pipe(concat('app.js'))
+//   .pipe(gulp.dest('js'));
+// });
+
+//check treehouse for examples on how to minify w/ uglify, rename, and compile sass with gulp-sass
